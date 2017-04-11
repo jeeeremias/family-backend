2706ea080d1f9dcd7ab38aa98cbf8c5935bb2a25
Jeremias Pereira <jeremias.livre@gmail.com>
2017-04-11 08:04:57 -0300

    Authentication middleware implemented

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var apiRoutes = express.Router(); 
 var app = express();
 
 // The only required route is for the calls to the mongopop API
 var kid = require('./controllers/kid');
+var health = require('./controllers/health');
 
 // View engine setup
 app.set('view engine', 'jade');
@@ -15,9 +17,22 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define and configure a rout middleware
+apiRoutes.use(function(req, res, next) {
+  if (req.headers['x-access-token']) {
+    next();
+  } else {
+    return res
+      .status(403)
+      .json({success: false, message: 'Failed to authenticate token.'});
+  }
+});
+
 // Define a single route; to be used to provide the Mongopop Restfull
 // API
+app.use('/', apiRoutes);
 app.use('/kid', kid);
+app.use('/health', health);
 
 // For any other routes, set the status to 404 and forward to error handler
 app.use(function(req, res, next) {
